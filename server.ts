c7a60540a0d06fd46d8f720b973f74ff48b95d6d
Herbert Yanes <rimgelair@gmail.com>
2022-05-03 03:47:42 -0400

    Updated dlpos-core to version 2.0.1 were the db params are passed to the createconnectionpool function

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import ReferenceDataService from "./src/app";
+import { createConnectionPool } from "dlpos-core";
 // import { initializeDb } from "dlpos-core";
 
 // initializeDb(`${__dirname}/resources/sql_scripts/refdata_tables.sql`).then(
@@ -12,6 +13,7 @@ import ReferenceDataService from "./src/app";
 //     }
 //   }
 // );
+createConnectionPool("dev_user", "localhost", "dl_staging", "password", 5432);
 ReferenceDataService.listen(5678, () => {
   console.log("DL Reference data service listening on port 5678!");
 });
